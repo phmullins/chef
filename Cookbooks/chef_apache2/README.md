7191297a7ac5c53422bd000cfb9945e5d55a5123
Patrick Mullins <pmullins11@mac.com>
2017-02-12 09:47:17 -0500

    Added cookbooks folder and first cookbook

---
+++ Cookbooks/chef_apache2/README.md
@@ -0,0 +1,3 @@
+# chef_apache2
+
+TODO: Installs the Apache 2 web server.
---
+++ Cookbooks/chef_apache2/metadata.rb
@@ -0,0 +1,19 @@
+name 'chef_apache2'
+maintainer 'Patrick H. Mullins'
+maintainer_email 'pmullins11@gmail.com'
+license 'all_rights'
+description 'Installs/Configures Apache 2'
+long_description 'Installs/Configures the Apache 2 web server'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/chef_apache2/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/chef_apache2' if respond_to?(:source_url)
